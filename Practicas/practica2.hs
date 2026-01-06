ed430cfad7059361e018b2a3ec883095a0170d0d
MartinianoOliveto <olivetomartiniano@gmail.com>
2026-01-06 09:36:38 -0500

    falta ejercicio 3

--- Practicas/practica2.hs
+++ Practicas/practica2.hs
@@ -85,3 +85,180 @@ agregar x [] = x
 agregar [] y = y
 --caso recursivo
 agregar (x:xs) ys = x : agregar xs ys 
+--13 
+reversa :: [a] ->[a]
+--caso base 
+reversa [] = []
+--caso recursivo
+reversa (x:xs) = agregarAlFinal (reversa xs) x 
+--14
+zipMaximos :: [Int] -> [Int] -> [Int]
+--caso base 
+zipMaximos [] [] = error "Ambas listas no pueden ser vacias"
+zipMaximos [] _ = [] --aca deberia devolver un error??
+zipMaximos _ [] = [] --aca deberia devolver un error??
+--caso recursivo 
+zipMaximos x y =  elMayorDe x : elMayorDe y : []
+
+elMayorDe :: [Int] -> Int 
+--dada una lista de numeros, devuelve el mayor 
+--PRECOND: La lista no puede ser vacia 
+--caso base 
+elMayorDe [] = error "La lista no puede ser vacia"
+elMayorDe (n:[]) = n --si solo hay un elemento, lo devuelvo   
+--caso recursivo
+elMayorDe (n:ns) = max n (elMayorDe ns) 
+--15 
+elMinimo :: Ord a => [a] -> a 
+--caso base 
+elMinimo [] = error "La lista no puede ser vacia"
+elMinimo (x:[]) = x 
+--caso recursivo
+elMinimo (x:xs) = min x (elMinimo xs)
+
+--2 recursion sobre numeros 
+--1
+factorial :: Int -> Int 
+--precondicion: n no puede ser negativo
+--caso base 
+factorial 0 = 1
+--caso recursivo
+factorial n = n * factorial(n-1)
+--2 
+cuentaRegresiva :: Int -> [Int]
+--caso base 
+cuentaRegresiva 0 = []
+--caso recursivo 
+cuentaRegresiva n = n : cuentaRegresiva (n-1)
+--3 
+repetir :: Int -> a -> [a]
+--caso base 
+repetir 0 x = []
+--caso recursivo 
+repetir x a = a : repetir (x-1) a
+--4 
+losPrimeros :: Int -> [a] -> [a]
+--caso base 
+losPrimeros _ [] = []
+--caso rescursivo
+losPrimeros n (x:xs) = if n > 0
+                        then x : losPrimeros (n-1) xs 
+                            else losPrimeros (n-1) xs 
+--5
+sinLosPrimeros :: Int -> [a] -> [a]
+--caso base 
+sinLosPrimeros 0 a = a
+--caso recursivo 
+sinLosPrimeros n (x:xs) = if n /= 0 
+                            then sinLosPrimeros (n-1) xs 
+                                else x : sinLosPrimeros (n-1) xs 
+
+--3 registros 
+data Persona = P String Int --nombre edad 
+    deriving Show 
+--personas 
+juan = P "Juan" 35 
+maria = P "Maria" 26 
+carlos = P "Carlos" 80
+--observadoras 
+edad :: Persona -> Int 
+edad (P n e) = e
+--1
+mayoresA :: Int -> [Persona] -> [Persona]
+--caso base 
+mayoresA _ [] = []
+--caso recursivo 
+mayoresA x (p:ps) = if edad p > x 
+                        then p : mayoresA x ps
+                            else mayoresA x ps 
+--2 
+promedioEdad :: [Persona] -> Int 
+--precondicion: la lista no puede ser vacia 
+--caso base 
+promedioEdad [] = error"La lista no puede ser vacia"
+--caso recursivo (no es recursivo jeje se encarga la subtarea)
+promedioEdad p = div (totalEdades p) (longitud p)
+
+totalEdades :: [Persona] -> Int 
+--dada una lista de personas devuelve el total de la suma de sus edades 
+--caso base 
+totalEdades [] = 0 
+--caso recursivo 
+totalEdades (p:ps) = edad p + totalEdades ps
+
+elMasViejo :: [Persona] -> Persona 
+--precondicion: la lista no es vacia 
+--caso base 
+elMasViejo [] = error"La lista no puede ser vacia"
+elMasViejo (p:[]) = p
+--caso recursivo
+elMasViejo (x:xs) = elMasViejoEntre x (elMasViejo xs) 
+
+elMasViejoEntre :: Persona -> Persona -> Persona 
+--dadas dos personas, devuelve la mas vieja 
+elMasViejoEntre x y = if edad x > edad y 
+                        then x 
+                            else y 
+--3.2
+data TipoDePokemon = Agua | Fuego | Planta
+    deriving Show 
+data Pokemon = ConsPokemon TipoDePokemon Int 
+    deriving Show 
+data Entrenador = ConsEntrenador String [Pokemon]
+    deriving Show 
+
+--pokemons de prueba 
+charmander = ConsPokemon Fuego 100
+squirtle = ConsPokemon Agua 75 
+bulbasaur = ConsPokemon Planta 50
+
+--Entrenadores 
+ash = ConsEntrenador "Ash" [charmander, squirtle] 
+red = ConsEntrenador "Red" [squirtle, bulbasaur, charmander]
+
+cantPokemon :: Entrenador -> Int 
+cantPokemon (ConsEntrenador _ []) = 0
+cantPokemon (ConsEntrenador _ lp) = longitud lp
+
+cantPokemonDe :: TipoDePokemon -> Entrenador -> Int 
+cantPokemonDe tp (ConsEntrenador _ (p:ps)) = contarPorTipo tp ps
+
+--cuantosDeTipo_De_LeGananATodosLosDe_ :: TipoDePokemon -> Entrenador -> Entrenador -> Int 
+
+esMaestroPokemon :: Entrenador -> Bool 
+esMaestroPokemon (ConsEntrenador _ ps) = tieneUnoDeCada ps 
+
+tieneUnoDeCada :: [Pokemon] -> Bool 
+--indica si en la lista hay al menos un pokemon de cada tipo 
+tieneUnoDeCada ps = hayUnoDe Fuego ps && hayUnoDe Planta ps && hayUnoDe Agua ps
+
+hayUnoDe :: TipoDePokemon -> [Pokemon] -> Bool
+--indica si hay al menos un pokemon del tipo dado en la lista 
+--caso base 
+hayUnoDe tp [] = False 
+--caso recursivo
+hayUnoDe tp (p:ps) = if not (esDeTipo tp p) 
+                        then hayUnoDe tp ps
+                            else True 
+--auxiliares
+esDeTipo :: TipoDePokemon -> Pokemon -> Bool
+--dado un TipoDePokemon y un Pokemon devuelve True si los tipos coinciden 
+esDeTipo Fuego (ConsPokemon Fuego _) = True   
+esDeTipo Planta (ConsPokemon Planta _) = True
+esDeTipo Agua (ConsPokemon Agua _) = True
+esDeTipo _ (ConsPokemon _ _) = False     
+
+contarPorTipo :: TipoDePokemon -> [Pokemon] -> Int 
+--dado un TipoDePokemon y una lista de Pokemon, devuelve la cantidad del TipoDePokemon que hay en la lista 
+--caso base 
+contarPorTipo tp [] = 0 
+--caso recursivo 
+contarPorTipo tp (p:ps) = if esDeTipo tp p 
+                            then 1 + contarPorTipo tp ps 
+                                else contarPorTipo tp ps 
+
+
+{-siempre en la recursion separo la cabeza de lista y trabajo con eso, entonces solo me queda saber que hago con el 
+resto de la lista, (que generalmente es aplicar la misma funcion, asi si funciona la logica de la cabeza de lista
+va a funcionar para el resto; otra cosa es darse cuenta donde usar la recursion, ya que esta puede ser realizada 
+por subtareas, tambien ver todos los casos base posibles (despues de hacer o plantear el recursivo)-}
