d9f34ead33abecda6a772168b7e7e1ab356f1edc
Martiniano Oliveto <olivetomartiniano@gmail.com>
2026-01-16 14:38:36 -0300

    hecho hasta el 9

--- Practicas/practica3.hs
+++ Practicas/practica3.hs
@@ -94,3 +94,84 @@ tesorosEn (ob:obs) = if esTesoro ob
 
 --2 tipos arboreos 
 --2.1 arboles binarios 
+data Tree a = EmptyT | NodeT a (Tree a) (Tree a)
+    deriving Show 
+--arboles de prueba 
+arbolSimple :: Tree Int --con esto se fuerza el tipo a Int, Haskell lo tomaba como Integer     
+arbolSimple = NodeT 10 
+                (NodeT 5 EmptyT EmptyT) 
+                    (NodeT 15 EmptyT EmptyT)
+arbolComplejo :: Tree Int
+arbolComplejo = NodeT 50
+                    (NodeT 25
+                        (NodeT 12 EmptyT EmptyT)
+                        (NodeT 35 EmptyT EmptyT)
+                    )
+                    (NodeT 75
+                        (NodeT 60 EmptyT EmptyT)
+                        (NodeT 90 EmptyT EmptyT)
+                    )
+arbolRepetidos :: Tree Int 
+arbolRepetidos = NodeT 0 
+                    (NodeT 0
+                        (NodeT 0 EmptyT EmptyT)
+                        (NodeT 0 EmptyT EmptyT)
+                    )
+                    (NodeT 0
+                        (NodeT 0 EmptyT EmptyT)
+                        (NodeT 0 EmptyT EmptyT)
+                    )
+arbolDispar :: Tree Int 
+arbolDispar = NodeT 100
+                (NodeT 50
+                    (NodeT 25
+                        (NodeT 12 EmptyT EmptyT) -- height de 4 
+                    EmptyT)
+                EmptyT)
+                (NodeT 150 EmptyT EmptyT) -- height de 2 
+
+--1                      
+sumarT :: Tree Int -> Int 
+sumarT EmptyT = 0
+sumarT (NodeT n t1 t2) = n + sumarT t1 + sumarT t2 
+--2
+sizeT :: Tree a -> Int 
+sizeT EmptyT = 0
+sizeT (NodeT _ t1 t2) = 1 + sizeT t1 + sizeT t2
+--3 
+mapDobleT :: Tree Int -> Tree Int 
+mapDobleT EmptyT = EmptyT 
+mapDobleT (NodeT n t1 t2) = (NodeT (n*2) (mapDobleT t1) (mapDobleT t2))
+--4 
+perteneceT :: Eq a => a -> Tree a -> Bool 
+perteneceT a EmptyT = False 
+perteneceT a (NodeT b t1 t2) = if a == b 
+                                then True 
+                                    else (perteneceT a t1) || (perteneceT a t2) 
+--5 
+aparicionesT :: Eq a => a -> Tree a -> Int 
+aparicionesT a EmptyT = 0
+aparicionesT a (NodeT b t1 t2) = if a == b 
+                                    then 1 + (aparicionesT a t1) + (aparicionesT a t2)
+                                        else (aparicionesT a t1) + (aparicionesT a t2)
+--6
+leaves :: Tree a -> [a]
+leaves EmptyT = []
+--si es un nodo con 2 hijos vacios es una hoja
+leaves (NodeT a EmptyT EmptyT) = [a]
+leaves (NodeT a t1 t2) = leaves t1 ++ leaves t2 
+--7 
+heightT :: Tree a -> Int 
+heightT EmptyT = 0 
+heightT (NodeT _ t1 t2) = 1 + max (heightT t1) (heightT t2)
+--8 
+mirrorT :: Tree a -> Tree a 
+mirrorT EmptyT = EmptyT 
+mirrorT (NodeT a t1 t2) = (NodeT a (mirrorT t2) (mirrorT t1))
+--9 
+toList :: Tree a -> [a]
+--primero se recorre la rama izq, raiz y derecha 
+toList EmptyT = []
+toList (NodeT n t1 t2) = toList t1 ++ n : toList t2 -- esto funciona, pero como sabe haskell como tiene que ordenarlo?
+--toList (NodeT n t1 t2) = toList t1 ... n ... toList t2 
+------------------------------------ ++ --- :: ----------- aca pense, si agrego n a t2 solo me queda hacer append con t1 
